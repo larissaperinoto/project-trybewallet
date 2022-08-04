12398bbe59d3b377fa0cc72faf6320b5a04c5146
Larissa Perinoto <perinotolarissa@gmail.com>
2022-08-04 12:16:12 -0300

    Adiciona folha de estilo no Header.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import './Header.css'
 
 class Header extends Component {
   render() {
     const { email, total } = this.props;
     return (
-      <div>
-        <span data-testid="email-field">{ email }</span>
-        <span data-testid="total-field">{ total }</span>
-        <span data-testid="header-currency-field"> BRL</span>
+      <div className="header_container">
+        <div className="logo_email_container">
+          <h2 className="title_header">Trybe<span>Wallet</span></h2>
+          <span data-testid="email-field" className="user_email">{ `Olá, ${email}` }</span>
+        </div>
+        <div className="total_container">
+          <span className="exit">Sair</span>
+          <span
+            data-testid="total-field"
+            className="total"
+          >
+            { `R$ ${total} BRL` }
+          </span>
+        </div>
       </div>
     );
   }
